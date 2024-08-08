dbc13d78762beba8df5f40be69357d206548a459
Last Chemist <lastchemist@gmail.com>
2024-08-08 17:48:43 +0330

    defined "read" method in class "Handler"

--- utils/JsonHandler.py
+++ utils/JsonHandler.py
@@ -8,8 +8,12 @@ class Handler:
         self.file_path: str = rf"{file_path}\data.json"
         if not os.path.isfile(self.file_path):
             self.write(content=self.content)
-    
-    def write(self,content:dict = {"equation_solution": "", "error_msg": ""}):
-        with open(self.file_path,"w") as file:
-            json.dump(content,file)
+
+    def write(self, content: dict = {"equation_solution": "", "error_msg": ""}):
+        with open(self.file_path, "w") as file:
+            json.dump(content, file)
+
+    def read(self):
+        with open(self.file_path, "r") as file:
+            self.content = json.loads(file)
     
