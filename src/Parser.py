33fafee52abc1059cf0d8080876d3d23426306f6
Last Chemist <lastchemist@gmail.com>
2024-08-08 18:10:16 +0330

    defined "class ElementMapper and init" method in "Parser.py"

--- src/Parser.py
+++ src/Parser.py
@@ -1,2 +1,12 @@
 import re
 from collections import Counter
+
+# region Element Mapper
+
+
+class ElementMapper:
+    def __init__(self, chemical_formula: str) -> None:
+        self.chemical_formula = chemical_formula
+
+
+# end region
