d177dc111da872fb402d409ff1d955aa7c3fadc5
Last Chemist <lastchemist@gmail.com>
2024-08-08 18:23:58 +0330

    defined "search" method in class "ElementMapper"

--- src/Parser.py
+++ src/Parser.py
@@ -1,12 +1,26 @@
 import re
 from collections import Counter
 
+
 # region Element Mapper
-
-
 class ElementMapper:
     def __init__(self, chemical_formula: str) -> None:
-        self.chemical_formula = chemical_formula
+        self.chemical_formula: str = chemical_formula
+
+    def search(self):
+        pattern: re.Pattern[str] = re.compile(
+            r"([A-Z][a-z]*)"
+        )  # Note : I don't know that using re.Pattern[str] is best practice or not!
+        found_elements: dict = {}
+        for match in pattern.finditer(self.chemical_formula):
+            element: str = match.group(1)
+            index: int = match.start()
+
+            if element in found_elements:
+                found_elements[element].append(index)
+            else:
+                found_elements[element] = [index]
+        return found_elements
 
 
 # end region
