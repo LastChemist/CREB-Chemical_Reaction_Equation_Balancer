bcf26ff5a149126d8a9188a6b80fc89b06ddc8cc
Last Chemist <lastchemist@gmail.com>
2024-08-09 15:46:04 +0330

    defined class "FileMaker and init"

--- src/LinearEquationsSystem.py
+++ src/LinearEquationsSystem.py
@@ -1,6 +1,6 @@
 from utils import Assets
 from Parser import ElementMapper, EquationParser
-
+import os
 
 # region Generator
 
@@ -101,6 +101,36 @@ class Generator:
             right_hand = ""
 
         return equations_list
+
+    # end region
+
+    # region Solver File maker
+
+    class FileMaker:
+        def __init__(self, chemical_equation: str) -> None:
+            self.chemical_equation = chemical_equation
+
+            self.current_directory = os.path.dirname(__file__)
+
+            equation_generator_object = Generator(
+                chemical_equation=self.chemical_equation
+            )
+            self.system_of_linear_equations: list = (
+                equation_generator_object.generateLinearEquationsSystem()
+            )
+
+            self.num_of_variables = (
+                equation_generator_object.demand_for_variables_to_solve_count
+            )
+            self.symbols_list = Assets.parameter_symbols
+            self.variables_str = ""
+
+            for i, symbol in enumerate(self.symbols_list):
+                self.variables_str += symbol
+                if self.num_of_variables == i:
+                    break
+                else:
+                    self.variables_str += ","
 
 
 # end region
