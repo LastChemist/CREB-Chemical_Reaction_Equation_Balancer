86dc5ba1bc34c36069faaea4320d5a51c058f696
Last Chemist <lastchemist@gmail.com>
2024-08-10 11:28:09 +0330

    missing method return types defined

--- utils/JsonHandler.py
+++ utils/JsonHandler.py
@@ -9,20 +9,20 @@ class Handler:
         if not os.path.isfile(self.file_path):
             self.write(content=self.content)
 
-    def write(self, content: dict = {"equation_solution": "", "error_msg": ""}):
+    def write(self, content: dict = {"equation_solution": "", "error_msg": ""})->None:
         with open(self.file_path, "w") as file:
             json.dump(content, file)
 
-    def read(self):
+    def read(self)->None:
         with open(self.file_path, "r") as file:
-            self.content = json.loads(file)
+            self.content = json.load(file)
 
-    def update(self, key: str, value: any):
+    def update(self, key: str, value)->None:
         self.read()
         self.content[key] = value
         self.write(content=self.content)
 
-    def removeJsonDataFile(self):
+    def removeJsonDataFile(self)->None:
         # the data.json is a temporary file, so after a successful execution 
         # of equation balance it would be removed
         if os.path.isfile(self.file_path):
