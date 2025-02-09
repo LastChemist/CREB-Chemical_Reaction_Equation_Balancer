f45760c2c5c6cfef1424726dfe148f421ee7ea55
Last Chemist <131850863+LastChemist@users.noreply.github.com>
2025-02-09 14:08:05 +0330

    just defined the classes

--- plugins/stoichiometry.py
+++ plugins/stoichiometry.py
@@ -1,3 +1,14 @@
 from Core.src import Parser, LinearEquationsSystem
 from Core.utils import ChemicalEquationRewriter, Assets
 
+
+class GlobalVariables:
+    pass
+
+
+class Stoichiometry:
+    pass
+
+
+class UI:
+    pass
